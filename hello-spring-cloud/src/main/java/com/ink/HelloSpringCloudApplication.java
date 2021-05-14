1f344c926c90eb998e7562e568138619d8db84ba
梅涛 <573945966@qq.com>
2021-05-14 17:01:28 +0800

    初始化Hello项目

---
+++ hello-spring-cloud/src/main/java/com/ink/HelloSpringCloudApplication.java
@@ -0,0 +1,13 @@
+package com.ink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringCloudApplication.class, args);
+    }
+
+}
