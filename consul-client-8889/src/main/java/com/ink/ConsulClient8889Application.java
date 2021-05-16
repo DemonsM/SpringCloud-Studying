1aa1630ab2a27b7bd1396f85d5d5a6237bdd8ca3
MT <573945966@qq.com>
2021-05-16 11:31:07 +0800

    Consul

---
+++ consul-client-8889/src/main/java/com/ink/ConsulClient8889Application.java
@@ -0,0 +1,13 @@
+package com.ink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsulClient8889Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsulClient8889Application.class, args);
+    }
+
+}
---
+++ consul-client-8889/src/test/java/com/ink/ConsulClient8889ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsulClient8889ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
