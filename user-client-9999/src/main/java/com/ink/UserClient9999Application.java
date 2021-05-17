a94626cd2758347e8487830548a9e36dd8b102a3
梅涛 <573945966@qq.com>
2021-05-17 14:12:10 +0800

    user client

---
+++ user-client-9999/src/main/java/com/ink/UserClient9999Application.java
@@ -0,0 +1,13 @@
+package com.ink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserClient9999Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserClient9999Application.class, args);
+    }
+
+}
